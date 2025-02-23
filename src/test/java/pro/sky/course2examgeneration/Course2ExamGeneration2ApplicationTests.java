35c8f1eea81ebd709833eb77d3eb3e688dcd7adc
Pavel-Kuznetsov-111 <gilrok@rambler.ru>
2025-02-23 22:29:11 +0300

    Проверка курсовой работы №2.

---
+++ src/test/java/pro/sky/course2examgeneration/Course2ExamGeneration2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.course2examgeneration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Course2ExamGeneration2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
